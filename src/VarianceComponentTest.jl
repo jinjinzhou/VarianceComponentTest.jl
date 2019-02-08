1249e454abff445436a66cae69019f5a161eb61f
jinjinzhou <jzhou@email.arizona.edu>
2019-02-07 21:39:23 -0700

    update to v1.0

--- src/VarianceComponentTest.jl
+++ src/VarianceComponentTest.jl
@@ -1,27 +1,12 @@
 module VarianceComponentTest
 
 import Distributions
-if VERSION < v"0.4-"
-       using Docile
-end
 
 export
-       readgeno!,
-       readAnnotate!,
-       kinshipcoef,
        vctestnullsim,
-       vctest,
-       loopwinFixsize,
-       loopwinAnnot,
-       gwasvctest
+       vctest
 
-include("readgeno!.jl")
-include("readAnnotate!.jl")
-include("kinshipcoef.jl")
 include("vctestnullsim.jl")
 include("vctest.jl")
-include("loopwinFixsize.jl")
-include("loopwinAnnot.jl")
-include("gwasvctest.jl")
 
 end
